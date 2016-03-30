f31f709e6fba6fb6c78be840d76f5c044ae5895e
Tyler Chapman <tyler@flurry.com>
2016-03-29 23:01:12 -0700

    fixing script to run the hql file

--- hql/experian/experian_update.sh
+++ hql/experian/experian_update.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-FILE=$1
-unzip $FILE'.zip'
-tail -n +2 $FILE'.txt' > $FILE.tmp && mv $FILE.tmp $FILE.'txt'
+
+FILEPATH=$1
+NAMENODE=$2
+FILE=${FILEPATH%.*}
+unzip $FILEPATH
+tail -n +2 $FILE.txt > $FILE.tmp && mv $FILE.tmp $FILE.txt
 
 hdfs dfs -rm /data/database/experian/originations/new/*
 hdfs dfs -put $FILE'.txt' /data/database/experian/originations/new/
+
+rm $FILE.txt
+
+if [ -z "$NAMENODE" ]
+then
+  echo "Cant find namenode add as second argument to run beeline"
+  exit 1
+fi
+
+/usr/hdp/current/spark-client/bin/beeline -u  jdbc:hive2://$NAMENODE:13001/ -f hql/experian/originations.hql
